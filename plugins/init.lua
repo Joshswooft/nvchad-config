891077377e7b14d19941ebd0017f964b76379072
Josh Dando <joshdando@MWG44P29PY.local>
2022-12-23 15:55:16 +0000

    feat: configure go lsp

--- plugins/init.lua
+++ plugins/init.lua
@@ -2,4 +2,11 @@ return {
     ["goolord/alpha-nvim"] = { 
         disable = false,
     }, -- enables dashboard
+
+    ["neovim/nvim-lspconfig"] = {
+        config = function()
+          require "plugins.configs.lspconfig"
+          require "custom.plugins.lspconfig"
+        end,
+    },
 }
